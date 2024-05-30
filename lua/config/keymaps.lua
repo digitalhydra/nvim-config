397073217c9c3edcb8e8892586347841fa3bd3df
digitalhydra <jimnoname@gmail.com>
2024-05-29 19:45:43 -0500

    more plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps hereby
+--
+-- Use K to show documentation in preview window
+-- nnoremap <silent> K :call <SID>show_documentation()<CR>
+vim.keymap.set("n", "<K>", '<Cmd>CocAction("doHover")<CR>')
